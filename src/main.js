a627ce3b027ec6d7fab64f0aed3c60ab6256e853
TheAsouka <TA@theworld.home>
2018-06-09 16:45:28 +0200

    flags

--- src/main.js
+++ src/main.js
@@ -6,31 +6,30 @@ function getJSON(yourUrl){
     return Httpreq.responseText;          
 }
 
-
 var json_obj = JSON.parse(getJSON("https://raw.githubusercontent.com/lsv/fifa-worldcup-2018/master/data.json"));
 //var s_json_obj = JSON.stringify(json_obj);
 console.log(json_obj);
-
 
 var txt = "";
 var letters= ["A","B","C","D","E","F","G","H"]
 y = 0;
 id = 0;
 
-txt += "<ul>"
-txt += "<li>Group"+" "+letters[y]+"</li>";
+txt += "<ul >"
+txt += "<li class='groups'>Group"+" "+letters[y]+"</li>";
 txt += "<ul>"
 for (var i = 0; i < json_obj.teams.length; i++){
-    
-    txt += "<li>" + json_obj.teams[i].name +" : "+ json_obj.teams[i].id + "</li>";
+    var idFlag = json_obj.teams[i].fifaCode;
+    idFlag = idFlag.toString().substring(0,2).toLowerCase();
+
+    txt += "<li class='teams'> <i class='flag-icon flag-icon-"+idFlag+"'></i>" + json_obj.teams[i].name +" : "+json_obj.teams[i].fifaCode+" : "+ json_obj.teams[i].id +"</li>";
+
     id += 1;
     if (id % 4 == 0 && id < 32){
         y += 1;
-        txt += "</ul><li>Group"+" "+letters[y]+"</li><ul>";
+        txt += "</ul><li class='groups'>Group"+" "+letters[y]+"</li><ul>";
     }
 }
 txt +="</ul></ul>"
-
 document.getElementById("output").innerHTML = txt;
 
-//document.getElementById("output").innerHTML = json_obj.teams[0].name;
