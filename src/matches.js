2dc195578fe801f68e7d9f3e271b075435cb4257
TheAsouka <TA@theworld.home>
2018-06-12 14:21:56 +0200

    TS

--- src/matches.js
+++ src/matches.js
@@ -2,17 +2,30 @@ console.log(json_obj);
 
 var txt = "";
 var lettersLower = ["a","b","c","d","e","f","g","h"];
+var y = 0;
 
-txt += "<tr> <th>Date</th> </tr>";
-y = 0;
+
+txt += "<tr><th>Group " + lettersLower[y].toUpperCase() + "</tr></th>"
+txt += "<tr> <th>Date</th> <th>Home</th> <th>Away</th> </tr>";
+
 
 for (var i = 0; i < 6; i++) {
-    txt += "<tr><td>" + eval("json_obj.groups."+lettersLower[y]+".matches["+i+"].date") + "</td></tr>";
     
-    if(i % 5 == 0){
-        //y += 1;
-        //i = 0;
-        //txt += "<tr><td>" + eval("json_obj.groups."+lettersLower[y]+".matches["+i+"].date") + "</td></tr>";
+    txt += "<tr><td>" + eval("json_obj.groups."+lettersLower[y]+".matches["+i+"].date") + "</td>";
+    txt += "<td>" + eval("json_obj.groups."+lettersLower[y]+".matches["+i+"].home_team") + "</td>";
+    txt += "<td>" + eval("json_obj.groups."+lettersLower[y]+".matches["+i+"].away_team") + "</td>";
+    txt += "</tr>";
+    
+    
+    if(i == 5 && y < 7){
+        y += 1;
+        i = 0;
+        txt += "<tr><th>Group " + lettersLower[y].toUpperCase() + "</tr></th>"
+        txt += "<tr> <th>Date</th> <th>Home</th> <th>Away</th> </tr>";
+        txt += "<tr><td>" + eval("json_obj.groups."+lettersLower[y]+".matches["+i+"].date") + "</td>";
+        txt += "<td>" + eval("json_obj.groups."+lettersLower[y]+".matches["+i+"].home_team") + "</td>";
+        txt += "<td>" + eval("json_obj.groups."+lettersLower[y]+".matches["+i+"].away_team") + "</td>";
+        txt += "</tr>";
     }
 }
 document.getElementById("outputM").innerHTML = txt;
